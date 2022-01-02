c0b352fb1bf2b10067d64d3d9d59e453fc1e898b
jjones254 <justinjones254@gmail.com>
2022-01-02 15:25:58 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const data = require('./data.json');
-
 const app = express();
+const { projects } = require('./data.json');
+
+
+
 
 // set view engine
 app.set('view engine', 'pug');
@@ -9,16 +11,7 @@ app.set('view engine', 'pug');
 app.use('/static', express.static('public'));
 
 app.get('/', (req, res) => {
-    res.render('index', data.projects);
-});
-
-app.get('/about', (req, res) => {
-    res.render('about');
-});
-
-app.get('/project/:id', (req, res) => {
-    let id = req.params.id;
-    res.render('project', data.projects[id]);
+    res.render('index', { projects });
 });
 
 app.listen(3000, () => {
