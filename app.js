bfc84fcb86f961346976ef0452916896c59a67cf
jjones254 <justinjones254@gmail.com>
2022-01-13 15:33:41 -0600

    Added code comments

--- app.js
+++ app.js
@@ -5,13 +5,11 @@ const app = express();
 app.set('view engine', 'pug');
 app.use('/static', express.static('public'));
 
+// use routes file
 const mainRoutes = require('./routes/main');
 app.use(mainRoutes);
 
-app.listen(3000, () => {
-    console.log("Listening on localhost:3000");
-});
-
+// 404 error handler
 app.use((req, res, next) => {
     const err = new Error('err');
     err.status = 404;
@@ -19,11 +17,17 @@ app.use((req, res, next) => {
     next(err);
 });
 
+// global error handler
 app.use((err, req, res, next) => {
     res.status(err.status || 500);
     res.send(err.message);
     console.log(err.status);
     console.log(err.message);
+});
+
+// serve the app on port 3000
+app.listen(3000, () => {
+    console.log("Listening on localhost:3000");
 });
 
 module.exports = app;
--- routes/main.js
+++ routes/main.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
+// use the data.json file
 const { projects } = require('../data.json');
 
+// render the home page
 router.get('/', (req, res) => {
     res.render('index', { projects });
 });
 
+// render the about page
 router.get('/about', (req, res) => {
     res.render('about');
 });
 
+// render the project by id
 router.get('/project/:id', (req, res, next) => {
     const projectId = req.params.id;
     const project = projects.find( ({ id }) => id === +projectId );
